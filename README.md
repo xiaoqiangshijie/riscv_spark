f2cff688edb88b3d8a088f08ebdf114134afa643
wangqiang <1062095934@qq.com>
2022-01-12 22:39:50 +0800

    git commit -m "add .s file test"

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+license:
 lmli2
+
+link:
+../../toolchains/riscv64-unknown-elf-gcc-8.3.0-2020.04.0-x86_64-linux-ubuntu14/bin/riscv64-unknown-elf-gcc -march=rv32im -mabi=ilp32 -mcmodel=medlow -ffunction-sections -fdata-sections -fno-builtin-printf -fno-builtin-malloc -T ../c_tests/link.lds -nostartfiles -Wl,--gc-sections -Wl,--check-sections a.out -o hello
---
+++ tests/s_tests/common.mk
@@ -0,0 +1,39 @@
+# 自动化编译脚本
+
+RISCV_PATH = ../../toolchains/riscv64-unknown-elf-gcc-8.3.0-2020.04.0-x86_64
+
+ifeq ($(system),win)
+GUN = $(RISCV_PATH)-w64-mingw32
+else ifeq ($(system),ubuntu)
+GUN = $(RISCV_PATH)-linux-ubuntu14
+else ifeq ($(system),mac)
+GUN = $(RISCV_PATH)-apple-darwin
+endif
+
+RISCV_GCC     = $(GUN)/bin/riscv64-unknown-elf-gcc
+RISCV_AS      = $(GUN)/bin/riscv64-unknown-elf-as
+RISCV_GXX     = $(GUN)/bin/riscv64-unknown-elf-g++
+RISCV_OBJDUMP = $(GUN)/bin/riscv64-unknown-elf-objdump
+RISCV_GDB     = $(GUN)/bin/riscv64-unknown-elf-gdb
+RISCV_AR      = $(GUN)/bin/riscv64-unknown-elf-ar
+RISCV_OBJCOPY = $(GUN)/bin/riscv64-unknown-elf-objcopy
+RISCV_READELF = $(GUN)/bin/riscv64-unknown-elf-readelf
+
+CFLAGS += -march=$(RISCV_ARCH)
+CFLAGS += -mabi=$(RISCV_ABI)
+CFLAGS += -mcmodel=$(RISCV_MCMODEL) -ffunction-sections -fdata-sections -fno-builtin-printf -fno-builtin-malloc
+
+LDFLAGS += -T ../c_tests/link.lds -nostartfiles -Wl,--gc-sections -Wl,--check-sections  # -T表示自己的链接脚本
+
+
+all: clean $(TARGET)
+
+$(TARGET):
+	$(RISCV_GCC) $(CFLAGS) -c ./smoke_add.S
+	$(RISCV_GCC) $(CFLAGS) $(LDFLAGS) smoke_add.o -o $@
+	$(RISCV_OBJCOPY) -O binary $@ $@.bin
+	$(RISCV_OBJDUMP) --disassemble-all $@ > $@.dump
+	python3 ../bin2mem.py $@.bin $@.mem
+
+clean:
+	rm -f $(TARGET) $(TARGET).dump $(TARGET).bin *.data *.o *.mem
