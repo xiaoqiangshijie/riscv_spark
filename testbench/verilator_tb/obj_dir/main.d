48b350fe8b2b93d394964ebb56818b34cccc9191
wangqiang <1062095934@qq.com>
2023-05-10 19:08:24 +0800

    add verilator makefile

--- testbench/verilator_tb/obj_dir/main.d
+++
@@ -1,7 +0,0 @@
-main.o: ../main.cpp /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_trace.h \
- /usr/share/verilator/include/verilated_trace_defs.h Vtop.h \
- /usr/share/verilator/include/verilated_heavy.h
